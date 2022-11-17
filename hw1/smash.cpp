f0eb11fe89982d05012ebe0ed84dac018a56bfe2
Sharon <sharonharari@campus.technion.ac.il>
2022-11-17 19:54:01 +0200

    first commit hw1

---
+++ hw1/smash.cpp
@@ -0,0 +1,90 @@
+/*	smash.c
+main file. This file contains the main function of smash
+*******************************************************************/
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include "commands.h"
+#include "signals.h"
+#include <iostream>
+#include <map>
+#include <string>
+#include <string_view>
+
+#define MAX_LINE_SIZE 80
+#define MAXARGS 20
+
+char* L_Fg_Cmd;
+void* jobs = NULL; //This represents the list of jobs. Please change to a preferred type (e.g array of char*)
+char lineSize[MAX_LINE_SIZE]; 
+
+enum job_state {
+	Stopped,
+	Running
+};
+
+struct job
+{
+	int pid;
+	std::string cmd;
+	job_state state;
+	int entered_time; // the time the job entered the list
+};
+
+int last_job;
+char * last_path;
+
+//**************************************************************************************
+// function name: main
+// Description: main function of smash. get command from user and calls command functions
+//**************************************************************************************
+int main(int argc, char *argv[])
+{
+    char cmdString[MAX_LINE_SIZE]; 	   
+
+	
+	//signal declaretions
+	//NOTE: the signal handlers and the function/s that sets the handler should be found in siganls.c
+	 /* add your code here */
+	
+	/************************************/
+	//NOTE: the signal handlers and the function/s that sets the handler should be found in siganls.c
+	//set your signal handlers here
+	/* add your code here */
+
+	/************************************/
+
+	/************************************/
+	// Init globals 
+
+    last_path = NULL;
+	
+	L_Fg_Cmd =(char*)malloc(sizeof(char)*(MAX_LINE_SIZE+1));
+	if (L_Fg_Cmd == NULL) 
+			exit (-1); 
+	L_Fg_Cmd[0] = '\0';
+	
+    	while (1)
+    	{
+	 	printf("smash > ");
+		fgets(lineSize, MAX_LINE_SIZE, stdin);
+		strcpy(cmdString, lineSize);    	
+		cmdString[strlen(lineSize)-1]='\0';
+					// perform a complicated Command
+		if(!ExeComp(lineSize)) continue; 
+					// background command	
+	 	if(!BgCmd(lineSize, jobs)) continue; 
+					// built in commands
+		ExeCmd(jobs, lineSize, cmdString);
+		
+		/* initialize for next line read*/
+		lineSize[0]='\0';
+		cmdString[0]='\0';
+	}
+    return 0;
+}
+
