3abefa87322fb7c46becd0b3e1d3846c9d9052df
FXP-Pscyhometery <37815312+gilkzxc@users.noreply.github.com>
2022-12-12 20:19:52 +0200

    Fixed Bank.cpp

--- hw2/Bank.cpp
+++ hw2/Bank.cpp
@@ -1,1 +1,52 @@
-#include <iostream>#include <fstream>#include <cstdint>#include <filesystem>#include <unistd.h>#include "Bank.h"namespace fs = std::filesystem;/**   Account ADT declaration*/Account::Account(int newBalance, int newPassword) :balance(newBalance), password(newPassword) {}Account::~Account() {}int Account::getBalance() const{    return *this->balance;}int Account::getPassword() const {    return *this->password;}bool Account::setBalance(int newBalance) :balance(newBalance) {}/**   Bank ADT declaration*/Bank::Bank() :profit(INITIAL_BANK_PROFIT) {}Bank::~Bank() {}//Gil needs to review/fix/update to avoid using unneeded syscallbool valid_args(int argc, char* argv[]) {     if (argc == 1) {        return false;    }    for (int i = 0; i < argc; i++) {        if (!fs::exists(argv[i])) {            return false;        }        if (!access(argv[i], R_OK)) {            std::perror();            return false;        }            }    return true;}int main(int argc, char* argv[]){    if (!valid_args(argc, argv)) {        return 1;    }    return 0;}
+#include <iostream>
+#include <fstream>
+#include <cstdint>
+#include <filesystem>
+#include <unistd.h>
+#include "Bank.h"
+namespace fs = std::filesystem;
+
+
+/*
+*	Account ADT declaration
+*/
+Account::Account(int newBalance, int newPassword) :balance(newBalance), password(newPassword) {}
+Account::~Account() {}
+int Account::getBalance() const {
+	return *this->balance;
+}
+int Account::getPassword() const {
+	return *this->password;
+}
+bool Account::setBalance(int newBalance) :balance(newBalance) {}
+
+
+/*
+*	Bank ADT declaration
+*/
+Bank::Bank() :profit(INITIAL_BANK_PROFIT) {}
+Bank::~Bank() {}
+
+//Gil needs to review/fix/update to avoid using unneeded syscall
+bool valid_args(int argc, char* argv[]) {
+	if (argc == 1) {
+		return false;
+	}
+	for (int i = 0; i < argc; i++) {
+		if (!fs::exists(argv[i])) {
+			return false;
+		}
+		if (!access(argv[i], R_OK)) {
+			std::perror();
+			return false;
+		}
+	}
+	return true;
+}
+
+int main(int argc, char* argv[]) {
+	if (!valid_args(argc, argv)) {
+		return 1;
+	}
+	return 0;
+}
